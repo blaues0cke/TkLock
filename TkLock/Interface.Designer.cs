65376786d51f1dc1d985def200e595d458cf78c5
blaues0cke <thomas@thomaskekeisen.de>
2013-08-04 14:19:54 +0200

    Initial commit.

---
+++ TkLock/Interface.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace TkLock
+{
+    partial class Interface
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Interface));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(229, 212);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(93, 378);
+            this.progressBar1.Maximum = 7;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(398, 78);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 2;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(375, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(285, 198);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(456, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 121);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ey! So einfach kommst du hier nicht rein!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 30000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 500;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Interface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(785, 521);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Interface";
+            this.Opacity = 0.9;
+            this.Text = "TkLock";
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.Load += new System.EventHandler(this.Interface_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+
+    }
+}
+
---
+++ TkLock/Interface.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Xml;
+
+using gma.System.Windows;
+
+
+
+namespace TkLock
+{
+
+    
+
+    public partial class Interface : Form
+    {
+
+        public string Password;
+        public int CurrentCharPos = 0;
+        public bool BruteForceBreak = false;
+        public int WrongTries = 0;
+        public int WrongTriesLimit = 20;
+        public String XmlFileName = "config.xml";
+
+        XmlDocument XmlFile;
+
+        public void CreateConfigFileIfNotExists()
+        {
+            XmlFile = new XmlDocument();
+            if (!File.Exists(XmlFileName))
+            {
+                XmlNode XmlTmpRoot, XmlTmpNode;
+                XmlTmpRoot = XmlFile.CreateElement("TkLock");
+
+
+                XmlTmpNode = XmlFile.CreateElement("Password");
+                XmlTmpNode.InnerText = "TkLock";
+                XmlTmpRoot.AppendChild(XmlTmpNode);
+                XmlTmpNode = XmlFile.CreateElement("Text1");
+                XmlTmpNode.InnerText = "Hör auf zu Probieren! Kleine Pause.";
+                XmlTmpRoot.AppendChild(XmlTmpNode);
+                XmlTmpNode = XmlFile.CreateElement("Text2");
+                XmlTmpNode.InnerText = "Richtig! Du darfst rein!";
+                XmlTmpRoot.AppendChild(XmlTmpNode);
+                XmlTmpNode = XmlFile.CreateElement("Text3");
+                XmlTmpNode.InnerText = "Schlecht...";
+                XmlTmpRoot.AppendChild(XmlTmpNode);
+                XmlTmpNode = XmlFile.CreateElement("Text4");
+                XmlTmpNode.InnerText = "Ja, weiter!";
+                XmlTmpRoot.AppendChild(XmlTmpNode);
+                XmlTmpNode = XmlFile.CreateElement("Text5");
+                XmlTmpNode.InnerText = "Ey! So einfach kommst du hier nicht rein!";
+                XmlTmpRoot.AppendChild(XmlTmpNode);
+
+                XmlFile.AppendChild(XmlTmpRoot);
+
+                XmlFile.Save(XmlFileName);
+
+
+
+
+            }
+            else
+            {
+                XmlFile.Load(XmlFileName);
+            }
+        }
+
+        public Interface()
+        {
+
+            InitializeComponent();
+            CreateConfigFileIfNotExists();
+            SetProgramSize();
+            SetElementPositions();
+
+
+            
+
+
+            label1.Text = XmlFile.SelectSingleNode("/TkLock/Text5").InnerText;
+            Password = XmlFile.SelectSingleNode("/TkLock/Password").InnerText;
+            progressBar1.Maximum = Password.Length;
+        }
+
+
+
+        public void CheckEntry(string Entry)
+        {
+            timer1.Enabled = false;
+            if (WrongTries == WrongTriesLimit)
+            {
+                timer2.Enabled = true;
+                CurrentCharPos = 0;
+                progressBar1.Value = 0;
+                WrongTries = 0;
+                SetText(XmlFile.SelectSingleNode("/TkLock/Text1").InnerText);
+                BruteForceBreak = true;
+            }
+            if (!BruteForceBreak)
+            {
+                if (CurrentCharPos < Password.Length && Password.Substring(CurrentCharPos, 1) == Entry)
+                {
+                    CurrentCharPos++;
+                    progressBar1.Value = CurrentCharPos;
+                    SetText(XmlFile.SelectSingleNode("/TkLock/Text4").InnerText);
+                }
+                else
+                {
+                    timer1.Enabled = true;
+                    CurrentCharPos = 0;
+                    progressBar1.Value = 0;
+                    WrongTries++;
+                    SetText(XmlFile.SelectSingleNode("/TkLock/Text3").InnerText);
+                }
+                if (CurrentCharPos == Password.Length)
+                {
+                    SetText(XmlFile.SelectSingleNode("/TkLock/Text2").InnerText);
+                    timer3.Enabled = true;
+                }
+            }
+        }
+
+
+        public void SetElementPositions()
+        {
+            pictureBox1.Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2 - pictureBox1.Width / 2, Screen.PrimaryScreen.Bounds.Height / 2 - pictureBox1.Height - progressBar1.Height);
+            progressBar1.Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2 - progressBar1.Width / 2, Screen.PrimaryScreen.Bounds.Height / 2 - progressBar1.Height + 50);
+            pictureBox2.Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2 + pictureBox1.Width / 2, Screen.PrimaryScreen.Bounds.Height / 2 - pictureBox1.Height - pictureBox2.Height - progressBar1.Height);
+            label1.Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2 + pictureBox1.Width / 2 + 85, Screen.PrimaryScreen.Bounds.Height / 2 - pictureBox1.Height - pictureBox2.Height - progressBar1.Height + 15);
+        }
+        public void SetProgramSize()
+        {
+            Size = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+        
+        }
+        public void SetText(string Text)
+        {
+            label1.Text = Text;
+        }
+
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SetText(XmlFile.SelectSingleNode("/TkLock/Text5").InnerText);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            BruteForceBreak = false;
+            SetText(XmlFile.SelectSingleNode("/TkLock/Text5").InnerText);
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        UserActivityHook actHook;
+
+        private void Interface_Load(object sender, EventArgs e)
+        {
+            
+            actHook = new UserActivityHook();
+            actHook.OnMouseActivity += new MouseEventHandler(MouseMoved);
+            actHook.KeyDown += new KeyEventHandler(MyKeyDown);
+            actHook.KeyPress += new KeyPressEventHandler(MyKeyPress);
+            actHook.KeyUp += new KeyEventHandler(MyKeyUp);
+        }
+        public void MyKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData.ToString() != "LShiftKey")
+            {
+                e.Handled = true;
+            }
+        }
+        public void MyKeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+            CheckEntry(e.KeyChar.ToString());
+            e.Handled = true;
+
+        }
+        public void MyKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData.ToString() != "LShiftKey")
+            {
+                  e.Handled = true;
+            }
+        }
+        public void MouseMoved(object sender, MouseEventArgs e)
+        {
+
+        }
+
+    }
+
+
+}
